a68753db877e9c1c6f76c12a6f1ec04082afde1f
Pablo Gómez-Caldito <pgomezcaldito@idealista.com>
2020-11-04 18:20:48 +0100

    Fix things

--- skeleton.m
+++ skeleton.m
@@ -173,7 +173,7 @@ end
 
 feat_mean_test = mean(features_test);
 feat_std_test  = std(features_test) ;
-features_test_n = (features_test - feat_mean_test)./feat_std_test;
+features_test_n = (features_test - feat_mean)./feat_std;
 
 check_normalization(features_test_n);
 
@@ -199,13 +199,13 @@ false_alarms = 0;
 for i = 1:length(labels_true)
     if labels_pred(i) == 1
         if labels_true(i) == 1
-            true_positgitives = true_positives + 1;
+            true_positives = true_positives + 1;
         else
             false_alarms = false_alarms + 1;
         end
     end
 end
-P_D  = true_positives / sum(labels_pred(:) == -1) ;
+P_D  = true_positives / sum(labels_pred(:) == 1) ;
 P_FA = false_alarms / sum(labels_pred(:) == -1) ;
 
 % Measure the performance of the developed system (AUC)
